208469c404752d37c7dd75717ab5b12a00da1243
overflaw <nicolas@devgiants.fr>
2024-10-17 14:50:53 +0200

    Add first minimal uncompiling test
    We start by introducing a dead-simple non-existing splitting string tool test.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_content_test() {
+        let mails: Vec<&str> = split("");
+        assert_eq!(mails.len(), 0);
+    }
+}
+
