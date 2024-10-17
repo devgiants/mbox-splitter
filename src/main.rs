7906036d34885405dafdc13dcbd74994fd047a73
overflaw <nicolas@devgiants.fr>
2024-10-17 15:08:02 +0200

    Make one-mail-only test pass

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() {}
 
-fn split(_mails_content: &str) -> Vec<&str> {
-    vec![]
+fn split(mails_content: &str) -> Vec<&str> {
+    let mut mails: Vec<&str> = Vec::new();
+    if mails_content.len() > 0 {
+        mails.push(mails_content);
+    }
+    mails
 }
 
 #[cfg(test)]
@@ -20,6 +24,7 @@ mod tests {
         let mail_content = fs::read_to_string("tests/data/one_mail.mbox")?;
         let mails: Vec<&str> = split(&mail_content);
         assert_eq!(mails.len(), 1);
+        assert_eq!(mails.get(0).unwrap(), &mail_content);
         Ok(())
     }
 }
