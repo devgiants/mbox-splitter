1ccd61b01612eb1be22fa89381b50703d7a0052a
overflaw <nicolas@devgiants.fr>
2024-10-17 15:02:34 +0200

    Add one-mail-only test.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-fn main() {
-
-}
+fn main() {}
 
 fn split(_mails_content: &str) -> Vec<&str> {
     vec![]
@@ -8,12 +6,21 @@ fn split(_mails_content: &str) -> Vec<&str> {
 
 #[cfg(test)]
 mod tests {
+    use std::{fs, io};
     use super::*;
 
     #[test]
     fn empty_content_test() {
         let mails: Vec<&str> = split("");
         assert_eq!(mails.len(), 0);
+    }
+
+    #[test]
+    fn one_mail_test() -> io::Result<()> {
+        let mail_content = fs::read_to_string("tests/data/one_mail.mbox")?;
+        let mails: Vec<&str> = split(&mail_content);
+        assert_eq!(mails.len(), 1);
+        Ok(())
     }
 }
 
