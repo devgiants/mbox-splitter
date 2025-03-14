d300ab7313e20dfa032720ca2f26264855e67c8d
overflaw <nicolas@devgiants.fr>
2025-03-14 15:48:25 +0100

    Start deep refactor
    Instead of loading the whole file and split it, we aim to
    to load predefined-length chunks of data, and start to look for separator
    from this point.
    
    This will ensure scalable handling for very large files
    
    Make empty content test pass. All others are red.

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,68 @@
 use std::fmt::format;
 use std::fs;
-use std::io::Error;
+use std::fs::File;
+use std::io::{BufReader, Error, Read, Seek, SeekFrom};
 
 fn main() {}
 
 const MBOX_MAIL_SEPARATOR: &str = "\nFrom ";
 
-fn split(mails_content: &str, chunk_size: usize) -> Vec<String> {
-    if mails_content.len() == 0 {
-        return vec![];
-    }
-    let mut mails: Vec<String> = Vec::new();
-    let mut current_length: usize = 0;
-    let mut mails_chunked = String::new();
+fn split_file(file_reader_ref: &mut File, chunk_size: u64) -> Vec<String> {
+    let mails: Vec<String> = Vec::new();
 
-    mails_content.split(MBOX_MAIL_SEPARATOR).for_each(|mail| {
-         if current_length > 0 && current_length + mail.len() > chunk_size {
-             mails.push(mails_chunked.clone());
-             mails_chunked = String::new();
-             current_length = 0;
-         }
-         mails_chunked += mail;
-         current_length += mail.len()
-    });
-     if current_length > 0 {
-         mails.push(MBOX_MAIL_SEPARATOR.to_owned() + &*mails_chunked.clone());
-     }
+
     mails
 }
 
 fn store(mails: Vec<String>) -> Result<Vec<String>, Error> {
     let mut mbox_paths: Vec<String> = Vec::new();
-    mails
-        .iter()
-        .for_each(|mail| {
-            let current_path = format!("tests/store/{}.mbox", mbox_paths.len());
-            fs::write(current_path.clone(), mail).unwrap();
-            mbox_paths.push(current_path);
-        });
+    mails.iter().for_each(|mail| {
+        let current_path = format!("tests/store/{}.mbox", mbox_paths.len());
+        fs::write(current_path.clone(), mail).unwrap();
+        mbox_paths.push(current_path);
+    });
     Ok(mbox_paths)
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{fs, io};
     use super::*;
+    use std::fs::File;
+    use std::{fs, io};
 
     const TEST_STORAGE_DIR: &str = "tests/store";
     #[test]
-    fn empty_content_test() {
-        let mails: Vec<String> = split("", 0);
+    fn empty_content_test() -> io::Result<()> {
+        let file_path = "tests/data/empty.mbox";
+        let mut file_reader_ref = File::open(file_path)?;
+        let mails: Vec<String> = split_file(&mut file_reader_ref, 80000);
         assert_eq!(mails.len(), 0);
+        Ok(())
     }
 
     #[test]
     fn one_mail_test() -> io::Result<()> {
-        let mail_content = fs::read_to_string("tests/data/one_mail.mbox")?;
-        let mails: Vec<String> = split(&mail_content, 100000);
+        let file_path = "tests/data/one_mail.mbox";
+        let mut file_reader_ref = File::open(file_path)?;
+        let mails: Vec<String> = split_file(&mut file_reader_ref, 100000);
         assert_eq!(mails.len(), 1);
         Ok(())
     }
 
     #[test]
     fn two_mails_test() -> io::Result<()> {
-        let mail_content = fs::read_to_string("tests/data/two_mails.mbox")?;
-        let mails: Vec<String> = split(&mail_content, 800);
+        let file_path = "tests/data/two_mails.mbox";
+        let mut file_reader_ref = File::open(file_path)?;
+        let mails: Vec<String> = split_file(&mut file_reader_ref, 800);
         assert_eq!(mails.len(), 2);
         Ok(())
     }
 
     #[test]
     fn split_by_size_test() -> io::Result<()> {
-        let mail_content = fs::read_to_string("tests/data/100_mails.mbox")?;
-        let mails: Vec<String> = split(&mail_content, 80000);
+        let file_path = "tests/data/100_mails.mbox";
+        let mut file_reader_ref = File::open(file_path)?;
+        let mails: Vec<String> = split_file(&mut file_reader_ref, 80000);
         assert_eq!(mails.len(), 2);
         Ok(())
     }
@@ -80,11 +70,15 @@ mod tests {
     #[test]
     fn store_mails_chunk() -> io::Result<()> {
         reinit_storage_dir()?;
-        let mail_content = fs::read_to_string("tests/data/100_mails.mbox")?;
-        let mails: Vec<String> = split(&mail_content, 80000);
+        let file_path = "tests/data/100_mails.mbox";
+        let mut file_reader_ref = File::open(file_path)?;
+        let mails: Vec<String> = split_file(&mut file_reader_ref, 80000);
         store(mails)?;
         for i in 0..=1 {
-            assert_eq!(fs::exists(format!("{}/{}.mbox", TEST_STORAGE_DIR, i))?, true);
+            assert_eq!(
+                fs::exists(format!("{}/{}.mbox", TEST_STORAGE_DIR, i))?,
+                true
+            );
         }
         Ok(())
     }
@@ -95,4 +89,3 @@ mod tests {
         Ok(())
     }
 }
-
